34f5206b1ead9ddee9441a72b99d9a15acbde5e2
Johnatan R <johnatan.r1000@gmail.com>
2023-04-29 17:20:30 -0500

    Se crea el layout y el navbar.

---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private state: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.state.select('products').subscribe({
+      next: (products) => {
+        console.log(products)
+      }
+    });
+  }
+
+}
