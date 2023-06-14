66068b6e239e1ce098526b9095c0a15d3738cb50
Jramos <johnatan.r1000@gmail.com>
2023-06-13 19:06:40 -0500

    Se redirecciona el modulo de los cursos y se cambia de carpeta.

--- src/app/modules/layout/layout-routing.module.ts
+++ src/app/modules/layout/layout-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 
-
-const routes: Routes = [{
-  path: '',
-  component: LayoutComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'courses',
+        // canActivate: [authGuardFn],
+        loadChildren: () =>
+          import('../courses/courses.module').then((m) => m.CoursesModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LayoutRoutingModule { }
+export class LayoutRoutingModule {}
