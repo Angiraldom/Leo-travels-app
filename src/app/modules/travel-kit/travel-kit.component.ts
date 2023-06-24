69ce1e8ff9ca243c6fac575c87d9c0998513bf9a
Jramos <johnatan.r1000@gmail.com>
2023-06-23 20:57:39 -0500

    Se crea el componente footer

--- src/app/modules/travel-kit/travel-kit.component.ts
+++ src/app/modules/travel-kit/travel-kit.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { BuyProductsComponent } from './buy-products/buy-products.component';
 import { DiscountSessionComponent } from './discount-session/discount-session.component';
 import { RecommendationSessionComponent } from './recommendation-session/recommendation-session.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-travel-kit',
   templateUrl: './travel-kit.component.html',
   styleUrls: ['./travel-kit.component.scss'],
   standalone: true,
-  imports: [CommonModule, BuyProductsComponent, DiscountSessionComponent, RecommendationSessionComponent],
+  imports: [CommonModule, BuyProductsComponent, DiscountSessionComponent, RecommendationSessionComponent, FooterComponent],
 })
 export default class TravelKitComponent {
 
