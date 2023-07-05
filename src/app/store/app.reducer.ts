6a20e2d25b736740015ef36f3f0324a66e8d5e38
Jramos <johnatan.r1000@gmail.com>
2023-07-05 15:49:53 -0500

    Se elimina componente que no estaba siendo usado.

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,6 +1,6 @@
 import { ActionReducerMap } from "@ngrx/store"
 import { cartReducer } from "./reducers/cart.reducer"
-import { ICart } from "../shopping-cart/interfaces/ICart.interface"
+import { ICart } from "../modules/products/interfaces/ICart.interface"
 import { profileReducer } from "./reducers/user.reducer"
 import { IUser } from "../modules/user/interfaces/IUser.interface"
 import { IStateCourse, courseViewReducer } from "./reducers/course.reducer"
