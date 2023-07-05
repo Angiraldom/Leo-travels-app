fc4f487af98038088cb8568269a37279c49aeade
Jramos <johnatan.r1000@gmail.com>
2023-07-05 00:59:59 -0500

    Se crea el componente de la promocion del curso.

---
+++ src/app/course-promotion/course-promotion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-promotion',
+  templateUrl: './course-promotion.component.html',
+  styleUrls: ['./course-promotion.component.scss']
+})
+export class CoursePromotionComponent {
+
+}
