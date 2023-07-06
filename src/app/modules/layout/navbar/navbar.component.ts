13cf4411419e99445e7f7ca97332ef2c6538baa7
Jramos <johnatan.r1000@gmail.com>
2023-07-05 19:13:39 -0500

    Se muestra el menu dependiendo del tipo de rol del usuario.

--- src/app/modules/layout/navbar/navbar.component.ts
+++ src/app/modules/layout/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { MaterialModule } from 'src/app/material/material.module';
 import { AppState } from 'src/app/store/app.reducer';
+import { IUser } from '../../admin/user/interfaces/IUser.interface';
 
 @Component({
   selector: 'app-navbar',
@@ -11,18 +13,22 @@ import { AppState } from 'src/app/store/app.reducer';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private state = inject(Store<AppState>);
 
-  constructor(private state: Store<AppState>) {}
-
-  user!: any;
+  user!: IUser;
+  $state!: Subscription;
 
   ngOnInit(): void {
-    this.state.select('profile').subscribe({
+    this.$state = this.state.select('profile').subscribe({
       next: (user) => {
         this.user = user;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.$state.unsubscribe();
   }
 
 }
--- src/app/modules/layout/sidebar/sidebar.component.ts
+++ src/app/modules/layout/sidebar/sidebar.component.ts
@@ -1,24 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { MaterialModule } from 'src/app/material/material.module';
 import { clearCart } from 'src/app/store/actions/cart.actions';
 import { AppState } from 'src/app/store/app.reducer';
+import { IUser } from '../../admin/user/interfaces/IUser.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MaterialModule, RouterModule],
+  imports: [MaterialModule, RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   private store = inject(Store<AppState>);
   private router = inject(Router);
+
+  user!: IUser;
+  $state!: Subscription;
 
   logout() {
     localStorage.clear();
     this.store.dispatch(clearCart());
     this.router.navigate(['login']);
+  }
+
+  ngOnInit(): void {
+    this.$state = this.store.select('profile').subscribe({
+      next: (user) => {
+        this.user = user;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.$state.unsubscribe();
   }
 }
