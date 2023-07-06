8732b46b18c89945f502b510cee513f5e5e5b2d0
Jorse <yorse57@gmail.com>
2023-07-05 20:40:31 -0500

    tarea finalizada listado de usuarios

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserComponent } from './user.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { UserRoutingModule } from './user-routing.module';
+import { HeaderModulesComponent } from "../../shared/header-modules/header-modules.component";
+import { MaterialModule } from 'src/app/material/material.module';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        UserComponent,
+        ListUsersComponent
+    ],
+    imports: [
+        CommonModule,
+        UserRoutingModule,
+        HeaderModulesComponent,
+        MaterialModule
+    ]
 })
 export class UserModule { }
