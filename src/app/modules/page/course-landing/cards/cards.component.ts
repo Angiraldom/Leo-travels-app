db3b56cadcefd7b48c1df2093ded3cc4fcd7795c
Jramos <johnatan.r1000@gmail.com>
2023-07-11 21:53:58 -0500

    Se remplazan los iconos por los links de aws.

--- src/app/modules/page/course-landing/cards/cards.component.ts
+++ src/app/modules/page/course-landing/cards/cards.component.ts
@@ -8,54 +8,59 @@ import { Component } from '@angular/core';
 export class CardsComponent {
   listCards = [
     {
-      image: 'assets/images/cards/IC-00.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-09.png',
       alt: 'IC-00',
       title: 'Introducción al curso',
     },
     {
-      image: 'assets/images/cards/IC-01.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-10.png',
       alt: 'IC-01',
       title: '1. Planifica tu viaje',
     },
     {
-      image: 'assets/images/cards/IC-02.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-11.png',
       alt: 'IC-02',
       title: '2. Cómo aprovechar el celular al máximo',
     },
     {
-      image: 'assets/images/cards/IC-03.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-12.png',
       alt: 'IC-03',
       title: '3. Resalta la belleza de cada paisaje',
     },
     {
-      image: 'assets/images/cards/IC-04.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-13.png',
       alt: 'IC-04',
       title: '4. La arquitectura y su majestuosidad',
     },
     {
-      image: 'assets/images/cards/IC-05.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-14.png',
       alt: 'IC-05',
       title: '5. Provoca con tus fotos de alimentos',
     },
     {
-      image: 'assets/images/cards/IC-06.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-15.png',
       alt: 'IC-06',
       title: '6. Poses creativas para tus fotos',
     },
     {
-      image: 'assets/images/cards/IC-07.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-16.png',
       alt: 'IC-07',
       title: '7. Color y vestimenta',
     },
     {
-      image: 'assets/images/cards/IC-08.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-17.png',
       alt: 'IC-08',
       title: '8. Los trucos de la edición',
     },
     {
-      image: 'assets/images/cards/IC-09.png',
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-18.png',
       alt: 'IC-09',
       title: '9. Publica tus mejores fotos en redes sociales',
+    },
+    {
+      image: 'https://vilean.s3.us-east-2.amazonaws.com/IMAGENES+SITIO+WEB/IC-19.png',
+      alt: 'IC-10',
+      title: 'Cierre del curso',
     },
   ];
 }
