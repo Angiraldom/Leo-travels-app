fce3f5e885fbc9ff062c1134f8e3e1313bda9961
Jramos <johnatan.r1000@gmail.com>
2023-07-25 23:13:48 -0500

    Se agrega la funcionalidad para calcular el descuento de cada precio.

---
+++ src/app/core/pipes/calculate-discount.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'calculateDiscount',
+  standalone: true
+})
+export class CalculateDiscountPipe implements PipeTransform {
+
+  transform(precioOriginal: number, porcentajeDescuento: number, amount: number): number {
+    precioOriginal *= amount;
+    porcentajeDescuento *= amount;
+    let descuento = (precioOriginal * porcentajeDescuento) / 100;
+    return precioOriginal - descuento;
+  }
+
+}
