65c2e430bfd4c8e267cf3a7d74f0d5f6b63f2a2c
Jramos <johnatan.r1000@gmail.com>
2023-07-26 15:16:59 -0500

    Se solucionan los bugs encontrados al momento de hacer el pago con wompi.

--- src/app/modules/page/purchase/invoice/invoice.component.ts
+++ src/app/modules/page/purchase/invoice/invoice.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -6,14 +6,33 @@ import { AppState } from 'src/app/store/app.reducer';
 import { TotalValuePipe } from 'src/app/core/pipes/total-values.pipe';
 import { FinalPricePipe } from 'src/app/core/pipes/final-price.pipe';
 import { CalculateDiscountPipe } from 'src/app/core/pipes/calculate-discount.pipe';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Subscription, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [CommonModule, TotalValuePipe, FinalPricePipe, CalculateDiscountPipe, MatTooltipModule],
+  imports: [CommonModule, TotalValuePipe, FinalPricePipe, CalculateDiscountPipe, MatTooltipModule, ReactiveFormsModule],
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss'],
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit, OnDestroy {
   store = inject(Store<AppState>);
+  totalValue = new FormControl(0);
+  $valueForm!: Subscription;
+  @Output() emitTotalValue = new EventEmitter();
+
+  ngOnInit(): void {
+    this.$valueForm = this.totalValue.valueChanges
+    .pipe(
+      distinctUntilChanged()
+    )
+    .subscribe((value) => {
+      this.emitTotalValue.emit(value);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.$valueForm.unsubscribe();
+  }
 }
