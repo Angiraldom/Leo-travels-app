bae007be17bc0c1bcb91f4ebc91db4b37208d207
Jramos <johnatan.r1000@gmail.com>
2023-07-27 19:46:19 -0500

    Se refactoriza el modulo de usuarios.

--- src/app/modules/admin/user/user.component.ts
+++ src/app/modules/admin/user/user.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './interface/IUser.interface';
+import { BaseClass } from 'src/app/core/base.class';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+export class UserComponent extends BaseClass implements OnInit {
+  user: IUser [] = [];
 
+  displayedColumns: string[] = ['document', 'full_name', 'email'];
+  dataSource: any;
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.baseService.getMethod('user/all').subscribe({
+      next: (response: any) => {
+        this.user = response.data;
+        this.dataSource = response.data;
+      },
+    });
+  }
 }
--- src/app/modules/admin/user/users.module.ts
+++ src/app/modules/admin/user/users.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
 import { HeaderModulesComponent } from 'src/app/shared/header-modules/header-modules.component';
-import { ListUsersComponent } from './list-users/list-users.component';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user-routing.module';
 
-
-
 @NgModule({
   declarations: [
-    ListUsersComponent,
     UserComponent
   ],
   imports: [
