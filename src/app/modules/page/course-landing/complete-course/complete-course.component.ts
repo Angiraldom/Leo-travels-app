2aaa0b9a3ca2a90e657cb3a02030eb3d651371ce
Jramos <johnatan.r1000@gmail.com>
2023-09-25 17:49:59 -0500

    Se finaliza la nueva version de la landing del curso.

---
+++ src/app/modules/page/course-landing/complete-course/complete-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complete-course',
+  templateUrl: './complete-course.component.html',
+  styleUrls: ['./complete-course.component.scss']
+})
+export class CompleteCourseComponent {
+
+}
---
+++ src/app/modules/page/course-landing/same-mistakes/same-mistakes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-same-mistakes',
+  templateUrl: './same-mistakes.component.html',
+  styleUrls: ['./same-mistakes.component.scss']
+})
+export class SameMistakesComponent {
+
+}
