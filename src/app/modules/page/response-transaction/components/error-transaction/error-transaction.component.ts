0637c48f5352f5140f9559229811319a4f7bc890
Jramos <johnatan.r1000@gmail.com>
2023-10-21 11:48:00 -0500

    Se agrega el boton de "volver al carrito" en la pagina de error de transaccion.

--- src/app/modules/page/response-transaction/components/error-transaction/error-transaction.component.ts
+++ src/app/modules/page/response-transaction/components/error-transaction/error-transaction.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { GenericButtonComponent } from 'src/app/shared/generic-button/generic-button.component';
 
 @Component({
   selector: 'app-error-transaction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GenericButtonComponent],
   templateUrl: './error-transaction.component.html',
   styleUrls: ['./error-transaction.component.scss'],
 })
-export class ErrorTransactionComponent {}
+export class ErrorTransactionComponent {
+  private router = inject(Router);
+
+  changeRoute() {
+    this.router.navigate(['pagar']);
+  }
+}
